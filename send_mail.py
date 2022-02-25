2b69045fe32607552531f02147cafa214142a1ca
vinsmazuka <distortion1988@mail.ru>
2022-02-25 13:24:49 +0900

    доб перем register_btn

--- send_mail.py
+++ send_mail.py
@@ -5,4 +5,6 @@ driver = webdriver.Chrome(executable_path='chromedriver.exe')
 
 driver.get('https://mail.ru/')
 
+register_btn = driver.find_element_by_xpath('//*[@id="mailbox"]/div[2]/a')
+register_btn.click()
 
