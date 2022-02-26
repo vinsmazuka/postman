b449bc81cc821a0d878a6eb80aac50203b983392
vinsmazuka <distortion1988@mail.ru>
2022-02-26 23:45:52 +0900

    доб заполнение формы данными

--- send_mail.py
+++ send_mail.py
@@ -5,6 +5,7 @@ from mimesis import Person
 from mimesis.locales import Locale
 from mimesis.enums import Gender
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -34,8 +35,8 @@ rand_surname = new_person.surname(gender=Gender.MALE)
 rand_day = str(random.randint(1, 28))
 rand_month = random.choice(months)
 rand_year = str(random.randint(1970, 2003))
-rand_account_name = ''.join(random.choice(account_name_symbols) for i in range(8))
-rand_password = ''.join(random.choice(password_symbols) for i in range(8))
+rand_account_name = ''.join(random.choice(account_name_symbols) for i in range(12))
+rand_password = ''.join(random.choice(password_symbols) for i in range(12))
 
 new_account = core.Account(
     name=rand_name,
@@ -59,11 +60,33 @@ register_btn.click()
 
 tabs = driver.window_handles
 driver.switch_to.window(tabs[1])
+driver.set_window_size(600, 700)
+
 name_field = driver.find_element(by=By.XPATH, value='//*[@id="fname"]')
 surname_field = driver.find_element(by=By.XPATH, value='//*[@id="lname"]')
+birth_day_selector = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div[3]/div[4]/div/div/div/div/form/div[5]/div[2]/div/div[1]/div/div/select")
+birth_month_selector = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div[3]/div[4]/div/div/div/div/form/div[5]/div[2]/div/div[3]/div/select")
+birth_year_selector = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div[3]/div[4]/div/div/div/div/form/div[5]/div[2]/div/div[5]/div/div/select")
+gender_selector = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div[3]/div[4]/div/div/div/div/form/div[8]/div[2]/div/label[1]/div[1]")
+account_name_field = driver.find_element(by=By.XPATH, value='//*[@id="aaa__input"]')
+password_field = driver.find_element(by=By.XPATH, value='//*[@id="password"]')
+create_btn = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/div[3]/div[4]/div/div/div/div/form/button')
+
 name_field.send_keys(rand_name)
 surname_field.send_keys(rand_surname)
-time.sleep(60)
+Select(birth_day_selector).select_by_visible_text(rand_day)
+Select(birth_month_selector).select_by_visible_text(rand_month)
+Select(birth_year_selector).select_by_visible_text(rand_year)
+gender_selector.click()
+account_name_field.send_keys(rand_account_name)
+password_field.send_keys(rand_password)
+time.sleep(2)
+repeat_password_field = driver.find_element(by=By.XPATH, value='//*[@id="repeatPassword"]')
+repeat_password_field.send_keys(rand_password)
+create_btn.click()
+
+
+time.sleep(30)
 driver.quit()
 
 
