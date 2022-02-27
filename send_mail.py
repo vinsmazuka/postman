cf45284265aeea18793a75a4a66baceb21490890
vinsmazuka <distortion1988@mail.ru>
2022-02-27 23:36:48 +0900

    доб отправку письма и создание архива

--- send_mail.py
+++ send_mail.py
@@ -1,3 +1,4 @@
+import os
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -30,6 +31,7 @@ tabs = driver.window_handles
 driver.switch_to.window(tabs[1])
 driver.set_window_size(600, 700)
 time.sleep(2)
+
 name_field = driver.find_element(by=By.XPATH, value='//*[@id="fname"]')
 surname_field = driver.find_element(by=By.XPATH, value='//*[@id="lname"]')
 birth_day_selector = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div[3]/div[3]/div[4]/div/div/div/div/form/div[5]/div[2]/div/div[1]/div/div/select")
@@ -48,10 +50,9 @@ Select(birth_year_selector).select_by_visible_text(new_account.birth_year)
 gender_selector.click()
 account_name_field.send_keys(new_account.account_name)
 password_field.send_keys(new_account.password)
-time.sleep(2)
+time.sleep(5)
 repeat_password_field = driver.find_element(by=By.XPATH, value='//*[@id="repeatPassword"]')
 repeat_password_field.send_keys(new_account.password)
-
 create_btn.click()
 time.sleep(10)
 
@@ -61,7 +62,6 @@ driver.switch_to.window(driver.window_handles[1])
 driver.execute_script("window.open()")
 driver.switch_to.window(driver.window_handles[-1])
 driver.get(str(img_link)+'.png')
-
 with open('captcha.png', 'wb') as file:
     file.write(driver.find_element(by=By.TAG_NAME, value='img').screenshot_as_png)
 driver.close()
@@ -83,9 +83,30 @@ time.sleep(20)
 
 cancel_btn = driver.find_element(by=By.XPATH, value='/html/body/div[16]/div[2]/div/div/div[2]/form/button[2]')
 cancel_btn.click()
+time.sleep(5)
 
-time.sleep(300)
+write_letter_btn = driver.find_element(by=By.XPATH, value='//*[@id="app-canvas"]/div/div[1]/div[1]/div/div[2]/span/div[1]/div[1]/div/div/div/div[1]/div/div/a/span/span')
+write_letter_btn.click()
+time.sleep(10)
+
+recipient_field = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div[3]/div[2]/div/div/div[1]/div/div[2]/div/div/label/div/div/input')
+recipient_field.send_keys('cto@groscloud.com')
+
+subject_field = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div[3]/div[3]/div[1]/div[2]/div/input')
+subject_field.send_keys('Тестовое задание Ивлев А.В.')
+
+message_field = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div[3]/div[5]/div/div/div[2]/div[1]')
+message_field.send_keys('Это те самые дроиды, которых вы ищите!\nИвлев Артем Владимирович, 89244019993')
+
+input_file_field = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div[3]/div[4]/div/div/div/button[1]/input')
+file = core.Archivator.make_archive(items=os.listdir())
+absolute_path = os.path.abspath(file)
+input_file_field.send_keys(absolute_path)
+time.sleep(120)
+
+send_mail_btn = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div/div[2]/div/div[2]/div[1]/span[1]')
+send_mail_btn.click()
+time.sleep(5)
 driver.quit()
-
 
 
