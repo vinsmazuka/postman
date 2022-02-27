fad102767b344017752d600bada4c3ac98be889b
vinsmazuka <distortion1988@mail.ru>
2022-02-27 23:47:25 +0900

    удал import Keys from selenium

--- send_mail.py
+++ send_mail.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
-from selenium.webdriver.common.keys import Keys
 import core
 
 rand_account = core.RandomAccount.generate()
